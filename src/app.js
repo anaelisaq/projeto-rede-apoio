5be766388b1680c7e0f7d9a5009ac3f4cbbd631b
anaelisaq <anaelisa.abs@gmail.com>
2022-07-29 23:57:55 -0300

    :heavy_plus_sign: build: add swagger

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require("dotenv-safe").config()
 const db = require("./database/mongoConfig")
 db.connect()
 
-//const swaggerDocument = require("")
+const swaggerDocument = require("./swagger.json")
 const professionalRouter = require("./routes/professionalRouter")
 const textRouter = require("./routes/textRouter")
 const index = require("./routes/indexRouter")
@@ -19,5 +19,7 @@ app.use(express.json())
 app.use("/", index)
 app.use("/professionals", professionalRouter)
 app.use("/texts", textRouter)
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 module.exports = app
